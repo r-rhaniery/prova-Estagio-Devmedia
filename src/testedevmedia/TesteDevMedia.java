efa4ab98d37499e223fbd2bd5dd757bfa73fa746
r-rhaniery <r.rhaniery@gmail.com>
2021-03-10 16:21:54 -0300

    atualização e correções de código

--- src/testedevmedia/TesteDevMedia.java
+++ src/testedevmedia/TesteDevMedia.java
@@ -5,55 +5,44 @@ import java.util.Scanner;
 public class TesteDevMedia {
 
 	public static void main(String[] args) {
-		
+
 		int codigoUsuario = 1234;
 		int codigoSenha = 9999;
-		
-		acesso log = new acesso();
+
 		Scanner in = new Scanner(System.in);
 		System.out.println("USUÁRIO: ");
-		log.usuario = in.nextInt();
-		
-		if (log.usuario != codigoUsuario) {
-				System.out.println("Usuário Invalido!");
-				System.out.println("Sistema Encerrado!");
-			
-		} else {
-			
-				Scanner pas = new Scanner(System.in);
-				System.out.println("SENHA: ");
-				log.senha = in.nextInt();
-				
-				if (log.senha == codigoSenha) {
-					System.out.println("Acesso Permitido!");
-				}
-				else {
-				
-					System.out.println("Senha Incorreta");
-					Scanner opcao = new Scanner(System.in);
-					System.out.println("Para tentar novamente: Aperte 1");
-					System.out.println("Para encerrar sistema: Aperte 0");
-					int escolha = in.nextInt();
-					
-						while (escolha == 1);{
-														
-						}
-					 	
-				  	if( escolha == 0){
-						System.out.println("Sistema Encerrado");
-					}
-				
-				else {
-					
-				}
+
+		var usuario = in.nextInt();
+
+		if (usuario != codigoUsuario) {
+			System.out.println("Usuário Inválido!");
+			System.out.println("Sistema Encerrado!");
+			System.exit(1);
+		}
+
+		var autenticado = false;
+
+		while (!autenticado) {
+			System.out.println("SENHA: ");
+			var senha = in.nextInt();
+
+			if (senha == codigoSenha) {
+				autenticado = true;
+
+				System.out.println("Usuário autenticado");
+				break;
+			}
+			System.out.println("Senha Inválida!");
+			System.out.println("Para tentar novamente: Aperte 1");
+			System.out.println("Para encerrar sistema: Aperte 0");
+			var opcao = in.nextInt();
+
+			if (opcao == 1)
+				continue;
+			else {
+				System.out.println("Sistema Encerrado");
+				break;
 			}
 		}
 	}
-}			
-			
-								
-					
-						
-					
-						
-		
+}
